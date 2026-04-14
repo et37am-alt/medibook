41de5d987d2aaf5e930d3da40f6f3eaed7f395a8
rahul <et37am@gmail.com>
2026-04-14 15:47:36 +0530

    Update MyAppointments.js

--- frontend/src/pages/MyAppointments.js
+++ frontend/src/pages/MyAppointments.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from "../api";
+import axios from "axios";
 import { toast } from 'react-toastify';
 axios.defaults.baseURL = "https://medibook-p82u.onrender.com";
 const STATUS = {
