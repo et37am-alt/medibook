aade80060db0f958927fa65dcf87fb5f02766d07
rahul <et37am@gmail.com>
2026-04-14 15:32:41 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { useAuth } from '../context/AuthContext';
-import axios from "axios";
-
+import axios from "../api";
 axios.defaults.baseURL = "https://medibook-p82u.onrender.com";
 const Login = () => {
   const [form, setForm]       = useState({ email: '', password: '' });
