91151fd0a2308c733ac8bb2d5b0c590c90e2b7f9
rahul <et37am@gmail.com>
2026-04-14 15:27:51 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import axios from "axios";
-
-axios.defaults.baseURL = "https://medibook-p82u.onrender.com";
+import axios from "../api";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
