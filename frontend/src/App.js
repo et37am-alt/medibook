b0b381f7961cb03b81879d55d10134d0492803d2
rahul <et37am@gmail.com>
2026-04-14 15:48:07 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ import Doctors         from './pages/Doctors';
 import BookAppointment from './pages/BookAppointment';
 import MyAppointments  from './pages/MyAppointments';
 import AdminDashboard  from './pages/AdminDashboard';
-import axios from "../api";
+import axios from "axios";
 // Redirect to login if not logged in
 const ProtectedRoute = ({ children }) => {
   const { user, loading } = useAuth();
