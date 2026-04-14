71ac5cec543b4e1dbaba9ba7f59bc940847a8eac
rahul <et37am@gmail.com>
2026-04-14 15:31:33 +0530

    Update BookAppointment.js

--- frontend/src/pages/BookAppointment.js
+++ frontend/src/pages/BookAppointment.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import DatePicker from 'react-datepicker';
-import axios from 'axios';
+import axios from "../api";
 import { toast } from 'react-toastify';
 
 axios.defaults.baseURL = "https://medibook-p82u.onrender.com";
